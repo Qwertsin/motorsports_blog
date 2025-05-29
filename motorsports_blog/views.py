747674b55f624dcfc862b9d66c2ab3cbce04f033
Qwertsin <157668769+Qwertsin@users.noreply.github.com>
2025-05-29 11:22:08 -0400

    small revisions

--- motorsports_blog/views.py
+++ motorsports_blog/views.py
@@ -1,6 +1,10 @@
-from django.http import HttpResponse
+"""Views for the motorsports blog application."""
+
 import datetime
+from django.http import HttpResponse
+
 
 def index(request):
     now = datetime.datetime.now()
+    # Return the home page response.
     return HttpResponse(f"Hello world! The current date and time is: {now}")
