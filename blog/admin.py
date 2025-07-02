205d807212478335b3ee4b159e22e3341e28f724
Qwertsin <157668769+Qwertsin@users.noreply.github.com>
2025-07-01 23:44:46 -0400

    Update admin.py
    Part 3 of assignment done (draft)

--- blog/admin.py
+++ blog/admin.py
@@ -3,6 +3,14 @@ from . import models
 
 # Register your models here.
 
+# tabular chosen due to models being short and compact
+class CommentInLine(admin.TabularInline):
+    model = models.Comment
+    extra = 0  # Do not show blank rows
+    fields = ('name', 'email', 'text', 'approved')  # Show only these
+    readonly_fields = ('name', 'email', 'text')  # These fields are read-only
+
+@admin.register(models.Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = (
         'title',
@@ -25,13 +33,34 @@ class PostAdmin(admin.ModelAdmin):
     )
 
     prepopulated_fields = {'slug': ('title',)}
+    inlines = [CommentInLine]   # Connects CommentInLine to PostAdmin
 
-admin.site.register(models.Post, PostAdmin)
 @admin.register(models.Topic)
-
 class TopicAdmin(admin.ModelAdmin):
     list_display = (
         'name',
         'slug',
     )
     prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(models.Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'email',
+        'approved',
+        'created',
+        'post',
+    )
+
+    search_fields = (
+        'name',
+        'email',
+        'text',
+    )
+
+    list_filter = (
+        'approved',
+    )
+
